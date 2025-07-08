eb21fead7904e98111fec909c501cef5cc056dea
cinny <band.maid@disroot.org>
2025-07-08 15:45:36 +0300

    Try to implement mouse tracker. Super inneficient needs rework.

---
+++ scripts/bodyTracker.js
@@ -0,0 +1,11 @@
+/* document.addEventListener('mousemove', (event) => {
+  const x = event.pageX;
+  const y = event.pageY;
+
+  const trail = document.createElement('div');
+  trail.classList.add('trail-x');
+  trail.style.left = `${x}px`;
+  trail.style.top = `${y}px`;
+
+  document.body.appendChild(trail);
+}) */
