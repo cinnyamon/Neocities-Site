2ce53d7c3072dcb0f0d5a8b8210f4511eef19d10
cinny <band.maid@disroot.org>
2025-07-13 23:54:12 +0300

    Add fade in/fade out on mouse enter and mouse leave respectively.

--- scripts/bodyTracker.js
+++ scripts/bodyTracker.js
@@ -1,14 +1,46 @@
-const mainEl = document.getElementById('gsapcursor')
+const mainEl = document.getElementById('gsapcursor');
+
 
 mainEl.addEventListener("mousemove", (event) => {
-  const x = event.clientX,
-  y = event.clientY;
+  const x = event.clientX - 6,
+  y = event.clientY - 6;
 
   // lag cursor behind mouse
   gsap.to("#cursor-follow > span", {
-      duration: (i) => 0.1 + i/10,
-      scale: (i) => 3 - i/10,
+      duration: (i) => 0.1 + i/10, // each span is called i and for each one we do: first span = 0 therefore i = 0 => 0.1 + 0/10 = 0.1 + 0 next is 1 which is 0.1 + 1/10 = 0.1 + 0.1 and so on
+      transform: (i) => `translate(${x}px, ${y}px) scale(${3 - i / 10})`,
       x,
       y
     });
 });
+
+mainEl.addEventListener("mouseenter", () => {
+  
+  gsap.killTweensOf(mainEl);
+  gsap.to("#cursor-follow > span", {
+      duration: 0.3,
+      opacity: 1,
+      ease: "power2.out"
+    });
+});
+
+mainEl.addEventListener("mouseleave", () => {
+
+  gsap.killTweensOf(mainEl);
+  gsap.to("#cursor-follow > span", {
+      duration: 0.3,
+      opacity: 0,
+      ease: "power2.out"
+    });
+});
+
+window.addEventListener('mouseout', (event) => {
+  if (!event.relatedTarget) {
+    gsap.killTweensOf(mainEl);
+    gsap.to("#cursor-follow > span", {
+        duration: 0.3,
+        opacity: 0,
+        ease: "power2.out"
+      });
+  }
+})
