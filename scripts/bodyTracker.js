d269b0ef4e0c94604235951ebf155a90d262e74e
cinny <band.maid@disroot.org>
2025-07-28 15:45:22 +0300

    pointerleave in a broken state.

--- scripts/bodyTracker.js
+++ scripts/bodyTracker.js
@@ -1,6 +1,8 @@
 // get DOM element
 const mainEl = document.getElementById('gsapcursor');
 const images = document.querySelectorAll('.crsr');
+const dotMask = document.querySelector('.dot-mask');
+
 
 // running a promise in order to let the dom load all the images for the cursor trail and then run the trail gsap code.
 const imagesLoaded = () => {
@@ -28,73 +30,52 @@ const imagesLoaded = () => {
 };
 
 imagesLoaded().then(() => {
-    document.addEventListener("mousemove", (event) => {
-      const x = event.clientX + 15,
-      y = event.clientY - 6;
+    let userMove;
 
-      // console.log('x:', x, 'y:', y)
-    
-        // lag cursor behind mouse
-      gsap.to("#cursor-follow > span", {
-        duration: (i) => 0.1 + i/10, // each span is called i and for each one we do: first span = 0 therefore i = 0 => 0.1 + 0/10 = 0.1 + 0 next is 1 which is 0.1 + 1/10 = 0.1 + 0.1 and so on
-        scale: (i) => 3 - i / 10,
-        x,
-        y
-      });
-        
-      gsap.to("#cursor-follow > span", {
-        rotation: 0,
-        duration: 0.8
-      });
+    mainEl.addEventListener("pointermove", event => {
+      userMove = true;
+      handleTrail(event, userMove);
+    });
+    mainEl.addEventListener("pointerenter", event => {
+      userMove = true;
+      handleTrail(event, userMove);
+      console.log('holding touch')
+    });
+    // problem child here this thing makes the cursor disappear instantly
+    mainEl.addEventListener('pointerleave', event => {
+      userMove = false;
+      handleTrail(event, userMove);
+      console.log('stopped holding')
     });
 
-    document.addEventListener('touchmove', (event) => {
-      const touch = event.targetTouches[0];
-      const x = touch.clientX + 15,
-      y = touch.clientY - 6;
 
-      // console.log('x:', x, 'y:', y)
+
+
+    /* mainEl.addEventListener('touchend', event => {
+      userMove = false;
+      handleTrail(event, userMove);
+      console.log('mouseleft')
+    }) */
+
     
-        // lag cursor behind mouse
-      gsap.to("#cursor-follow > span", {
-        duration: (i) => 0.1 + i/10, // each span is called i and for each one we do: first span = 0 therefore i = 0 => 0.1 + 0/10 = 0.1 + 0 next is 1 which is 0.1 + 1/10 = 0.1 + 0.1 and so on
-        scale: (i) => 3 - i / 10,
-        x,
-        y
-      });
-        
-      gsap.to("#cursor-follow > span", {
-        rotation: 0,
-        duration: 0.8
-      });
-    })
-    
-    document.addEventListener("mouseenter", (event) => {
-      
-      // gsap.killTweensOf(mainEl);
-      gsap.to("#cursor-follow > span", {
-          duration: 0.3,
-          opacity: 1,
-          ease: "power2.out"
-        });
+
+    /* mainEl.addEventListener('touchmove', event => {
+      userMove = true;
+      handleTrail(event, userMove);
     });
+    mainEl.addEventListener('touchstart', event => {
+      userMove = true;
+      handleTrail(event, userMove);
+      console.log('holding touch')
+    });
+    mainEl.addEventListener('touchend', event => {
+      userMove = false;
+      handleTrail(event, userMove);
+    }) */
+   
     
-    document.addEventListener('mouseleave', (event) => {
-      if (!event.relatedTarget) {
-        // gsap.killTweensOf(mainEl);
-        gsap.to("#cursor-follow > span", {
-            duration: 0.3,
-            opacity: 0,
-            ease: "power2.out"
-          });
-      }
-    })
-    
-
-    
-    isContextMenuOpen = 0
-    
-    document.addEventListener('contextmenu', (e) => {
+    isContextMenuOpen = 0;
+    document.addEventListener('contextmenu', () => {
       isContextMenuOpen++
       // e.preventDefault();
       gsap.to("#cursor-follow > span", {
@@ -109,44 +90,178 @@ imagesLoaded().then(() => {
         isContextMenuOpen = 0;
       }
     })  
-})
+});
 
+const handleTrail = (event, userMove) => {
+  if (event.pointerType === 'mouse') {
+    const x = event.clientX + 15,
+    y = event.clientY - 6;
 
-document.addEventListener('DOMContentLoaded', () => {
-    const dotMask = document.querySelector('.dot-mask');
+    moveXTrail(x, y, userMove);
+    backgroundTrail(event, userMove)
 
-    if (dotMask) {
-      let targetX = 0, targetY = 0;
-      let currentX = 0, currentY = 0;
+  } else if (event.pointerType === 'touch') {
+    const x = event.clientX + 15,
+    y = event.clientY - 6;
 
-      document.addEventListener('mousemove', (e) => {
-        /* targetX = e.clientX;
-        targetY = e.clientY; */
-        targetX = e.pageX;
-        targetY = e.pageY;
-      });
+    console.log(userMove)
+    moveXTrail(x, y, userMove);
+    backgroundTrail(event, userMove)
+  }
+}
 
-      document.addEventListener('mouseout', (e) => {
-        dotMask.style = `mask-image: radial-gradient(ellipse 350px 350px at ${currentX}px ${currentY}px, rgba(0, 0, 0, 0) 10%, transparent 100%)`;
-      })
+const moveXTrail = (x, y, userMove) => {
+  if (userMove) {
+    // opacity 1 on all viewport leave events
+    // lag cursor behind mouse
+    gsap.to("#cursor-follow > span", {
+      duration: (i) => 0.1 + i/10, // each span is called i and for each one we do: first span = 0 therefore i = 0 => 0.1 + 0/10 = 0.1 + 0 next is 1 which is 0.1 + 1/10 = 0.1 + 0.1 and so on
+      scale: (i) => 3 - i / 10,
+      x,
+      y
+    });
+    gsap.to("#cursor-follow > span", {
+      rotation: 0,
+      duration: 0.8
+    });
+    gsap.to("#cursor-follow > span", {
+      duration: 0.3,
+      opacity: 1
+    })
+  } else {
+    // opacity 0 on all viewport leave events
+    gsap.to("#cursor-follow > span", {
+      duration: (i) => 0.1 + i/10,
+      scale: (i) => 3 - i / 10,
+      x,
+      y
+    });
+    gsap.to("#cursor-follow > span", {
+      rotation: 0,
+      duration: 0.8
+    });
+    gsap.to("#cursor-follow > span", {
+      duration: 0.3,
+      opacity: 0
+    })
+  }
+}
 
-      document.addEventListener('touchmove', (e) => {
-        const touch = e.targetTouches[0];
-        targetX = touch.clientX;
-        targetY = touch.clientY;
-      })
+const backgroundTrail = (event, userMove) => {
+  const x = event.pageX - 10,
+    y = event.pageY - 10;
 
-      function animate() {
-        currentX += (targetX - currentX) * 0.1;
-        currentY += (targetY - currentY) * 0.1;
+  // const rect = dotMask.getBoundingClientRect();
+  // const x = event.pageX - (rect.left + window.scrollX) - 10;
+  // const y = event.pageY - (rect.top + window.scrollY) - 10;
 
-        dotMask.style = `mask-image: radial-gradient(ellipse 300px 300px at ${currentX}px ${currentY}px, rgba(0, 0, 0, 1) 10%, transparent 100%)`;
-
-        requestAnimationFrame(animate);
+  if (userMove) {
+    // animate bg mask on mousemove
+    gsap.to(dotMask, {
+      ease: "power3.out",
+      onUpdate() {
+        const maskGradient = `radial-gradient(ellipse 200px 200px at ${x}px ${y}px, rgb(0, 0, 0) 10%, transparent 100%)`;
+        dotMask.style.maskImage = maskGradient
+        dotMask.style.webkitMaskImage = maskGradient
       }
+    });
+    //animate bg mask opacity
+    gsap.to(dotMask, {
+      duration: 0.6,
+      opacity: 1
+    })
+  } else {
+    // animate bg mask on mousemove
+    gsap.to(dotMask, {
+      ease: "power3.out",
+      onUpdate() {
+        const maskGradient = `radial-gradient(ellipse 200px 200px at ${x}px ${y}px, rgb(0, 0, 0) 10%, transparent 100%)`;
+        dotMask.style.maskImage = maskGradient
+        dotMask.style.webkitMaskImage = maskGradient
+      }
+    });
+    //animate bg mask opacity
+    gsap.to(dotMask, {
+      duration: 0.6,
+      opacity: 0
+    })
+  }
+  
+  
+  
+  
+  
+  /* const touch = event.targetTouches[0];
+  const x = touch.clientX + 15,
+    y = touch.clientY - 6;
 
-      animate();
-
-      
-    }
+    // lag cursor behind mouse
+  gsap.to("#cursor-follow > span", {
+    duration: (i) => 0.1 + i/10, // each span is called i and for each one we do: first span = 0 therefore i = 0 => 0.1 + 0/10 = 0.1 + 0 next is 1 which is 0.1 + 1/10 = 0.1 + 0.1 and so on
+    scale: (i) => 3 - i / 10,
+    x,y,
   });
+    
+  gsap.to("#cursor-follow > span", {
+    rotation: 0,
+    duration: 0.8
+  }); */
+}
+/* function handleTrailOpacity(opacity) {
+  if (userMove) {
+    // gsap.killTweensOf(mainEl);
+    gsap.to("#cursor-follow > span", {
+      duration: 0.3,
+      opacity: opacity,
+      ease: "power2.out"
+    });
+  } else {
+    // gsap.killTweensOf(mainEl);
+    gsap.to("#cursor-follow > span", {
+      duration: 0.3,
+      opacity: opacity,
+      ease: "power2.out"
+    });
+
+    console.log(touching)
+  }
+} */
+
+/* function backgroundTrail(event, userMove, opacity) {
+  if (userMove){
+    const x = event.pageX - 15,
+      y = event.pageY - 10;
+
+    // animate bg mask on mousemove
+    gsap.to(dotMask, {
+      ease: "power3.out",
+      opacity: 1,
+      onUpdate() {
+        const maskGradient = `radial-gradient(ellipse 200px 200px at ${x}px ${y}px, rgb(0, 0, 0) 10%, transparent 100%)`;
+        dotMask.style.maskImage = maskGradient
+        dotMask.style.webkitMaskImage = maskGradient
+      }
+    });
+  } else {
+    const touch = event.targetTouches[0];
+    const x = touch.pageX - 15,
+      y = touch.pageY - 10;
+
+    // animate bg mask on mousemove
+    gsap.to(dotMask, {
+      ease: "power3.out",
+      opacity: 1,
+      onUpdate() {
+        const maskGradient = `radial-gradient(ellipse 200px 200px at ${x}px ${y}px, rgb(0, 0, 0) 10%, transparent 100%)`;
+        dotMask.style.maskImage = maskGradient
+        dotMask.style.webkitMaskImage = maskGradient
+      }
+    });
+  }
+}
+ */
+
+
+document.addEventListener("pointermove", (event) => {
+/*   console.log(event.pointerType)
+ */});
